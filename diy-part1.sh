8becc01427898d36d5629078f3c598ce86075108
linyuhc001 <linyuhc001@gmail.com>
2021-09-17 12:26:33 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -288,6 +288,7 @@ CONFIG_PACKAGE_kmod-tun=y
 CONFIG_PACKAGE_libcap=y
 CONFIG_PACKAGE_libcap-bin=y
 # CONFIG_PACKAGE_ip6tables-mod-nat=y
+CONFIG_PACKAGE_qemu-ga=y
 CONFIG_PACKAGE_iptables-mod-extra=y
 CONFIG_PACKAGE_ttyd=y
 EOF
